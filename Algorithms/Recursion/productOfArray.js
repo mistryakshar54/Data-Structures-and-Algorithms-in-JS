f7705274bcbb4d681ca85c7b7b9c780704a43523
Akshar Mistry <mistry.aks@gmail.com>
2020-04-28 18:58:17 +0530

    Create productOfArray.js

---
+++ Algorithms/Recursion/productOfArray.js
@@ -0,0 +1,11 @@
+// Write a function called `productOfArray` which takes in
+// an array of numbers and returns the product of them all.
+
+const productOfArray = (arr) => {
+  if (arr.length === 0) {
+    return 1;
+  }
+  return arr[0] * productOfArray(arr.slice(1));
+};
+
+console.log(productOfArray([1, 20, 3, 10]));
