0ac7d65c5c4db68f7b1d9930e97eb4c4287aa955
Akshar Mistry <mistry.aks@gmail.com>
2020-10-15 13:53:49 +0530

    Create Permutation.js

---
+++ Algorithms/Recursion/Permutation.js
@@ -0,0 +1,21 @@
+//Find permutation of array elements
+
+const swap = (arr, a, b) => {
+  let temp = arr[a];
+  arr[a] = arr[b];
+  arr[b] = temp;
+};
+
+const findPerm = (arr, l, h) => {
+  if (l === h) {
+    console.log(arr);
+  }
+
+  for (let i = l; i < arr.length; i++) {
+    swap(arr, i, l);
+    findPerm(arr, l + 1, h);
+    swap(arr, i, l);
+  }
+};
+
+findPerm(["A", "B", "C"], 0, 2);
